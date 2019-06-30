3a509fc4f2ed7495e5f072f8bc679f377a47ca6b
Subir Chandra Ghosh <subir0071@users.noreply.github.com>
2019-06-30 16:53:23 +0530

    Update printStep.groovy

--- example/printStep.groovy
+++ example/printStep.groovy
@@ -1,6 +1,6 @@
 void call(){
-    for(def i = 0; i < ${config.number}; i++){
-        println ${config.number}
+    for(def i = 0; i < config.number; i++){
+        println config.number
     }
 }
 
