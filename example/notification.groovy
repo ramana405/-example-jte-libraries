48e6c7da9a589d50f74568508860818542f71e2f
Subir Chandra Ghosh <subir0071@users.noreply.github.com>
2019-07-01 10:12:56 +0530

    Update notification.groovy

--- example/notification.groovy
+++ example/notification.groovy
@@ -1,6 +1,7 @@
 @AfterStep
 void call(context){
-
+echo "context step " + context.step
+echo "context status "+ context.status
     if (context.step.equals("build") && context.status.equals("FAILURE")){
         //slackSend color: '#ff0000', message: "Build Failure: ${env.JOB_URL}"
         echo "inside method of notification"
