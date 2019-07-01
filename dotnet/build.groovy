2d812395021901f55d1a8546c419a8408a59932f
Subir Chandra Ghosh <subir0071@users.noreply.github.com>
2019-07-01 12:36:02 +0530

    Update build.groovy

--- dotnet/build.groovy
+++ dotnet/build.groovy
@@ -1,9 +1,13 @@
 void call() {
 echo "starting msbuild"
+stage('build') {
+       echo "Helloooooooooooooooooo"
+       bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
 
-bat '"C:\\ProgramData\\chocolatey\\lib\\NuGet.CommandLine\\tools\\nuget.exe" restore DancingGoat.sln'
+       bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
+ 
+  } 
 
-bat "\"${tool 'MSBuild'}\" DancingGoat.sln /p:Configuration=Release /p:Platform=\"Any CPU\" /p:ProductVersion=1.0.0.${env.BUILD_NUMBER}"
   
 echo "ending msbuild"
 }
