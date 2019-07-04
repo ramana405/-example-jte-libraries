23bbd0f8533ebfaf8c2df42419fb70ffe6f40776
ramana405 <41909526+ramana405@users.noreply.github.com>
2019-07-04 15:12:49 +0530

    Update Deploy.groovy

--- dotnet/Deploy.groovy
+++ dotnet/Deploy.groovy
@@ -10,7 +10,7 @@ stage("Deploy"){
               mail to: 'venkataramana.thalari@cummins.com',
               subject: "Job '${env.JOB_NAME}' (${env.BUILD_NUMBER}) is waiting for input",
               body: "Please go to ${env.BUILD_URL}console and verify the build"
-				      input "Deploy to Production?"
+              input "Deploy to Production?"
 				      //Deployment Script
         }
         post{
